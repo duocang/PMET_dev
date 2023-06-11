40f3c491992638db12c3728ad68c48d8a944304c
Bai juyi <wangxuesong29@gmail.com>
2023-06-11 20:34:55 +0200

    code style format

--- src/pmetParallel/Output.hpp
+++ src/pmetParallel/Output.hpp
@@ -11,38 +11,36 @@
 
 #include "motifComparison.hpp"
 
-class Output
-{
-    friend bool sortComparisons(const Output &a, const Output &b);
+class Output {
+  friend bool sortComparisons(const Output &a, const Output &b);
 
 public:
-    Output(){};
-    Output(const std::string &m1name, const std::string &m2name, const motifComparison &mComp)
-    {
-        motif1Name = m1name;
-        motif2Name = m2name;
+  Output(){};
+  Output(const std::string &m1name, const std::string &m2name, const motifComparison &mComp) {
+    motif1Name = m1name;
+    motif2Name = m2name;
 
-        pval = mComp.getpValue();
-        clusterSize = mComp.getClusterSize();
+    pval = mComp.getpValue();
+    clusterSize = mComp.getClusterSize();
 
-        genesInClusterWithBothMotifs = mComp.getSharedGenesInCluster();
-        numSharedGenesInUniverse = mComp.getNumSharedGenesInUniverse();
-    }
+    genesInClusterWithBothMotifs = mComp.getSharedGenesInCluster();
+    numSharedGenesInUniverse = mComp.getNumSharedGenesInUniverse();
+  }
 
-    static void writeHeaders(std::ofstream &of);
-    void printMe(long globalBonferroniFactor, std::ofstream &outFile);
-    double getpValue() const { return pval; };
-    void setBHCorrection(double p) { pvalBHCorrected = p; };
+  static void writeHeaders(std::ofstream &of);
+  void printMe(long globalBonferroniFactor, std::ofstream &outFile);
+  double getpValue() const { return pval; };
+  void setBHCorrection(double p) { pvalBHCorrected = p; };
 
 private:
-    std::string motif1Name;
-    std::string motif2Name;
-    double pval;
-    double pvalBHCorrected;
-    long clusterSize;
+  std::string motif1Name;
+  std::string motif2Name;
+  double pval;
+  double pvalBHCorrected;
+  long clusterSize;
 
-    std::vector<std::string> genesInClusterWithBothMotifs;
-    long numSharedGenesInUniverse;
+  std::vector<std::string> genesInClusterWithBothMotifs;
+  long numSharedGenesInUniverse;
 };
 
 bool sortComparisons(const Output &a, const Output &b);
--- src/pmetParallel/motifComparison.hpp
+++ src/pmetParallel/motifComparison.hpp
@@ -9,37 +9,39 @@
 #ifndef motifComparison_hpp
 #define motifComparison_hpp
 
+#include <fstream>
 #include <string>
 #include <vector>
-#include <fstream>
+
 #include "motif.hpp"
 
-class motifComparison
-{
-
-    // represents one line in the results file
+class motifComparison {
+  // represents one line in the results file
 
 public:
-    double getpValue() const { return pval; };
-    long getClusterSize() const { return clusterSize; };
-    std::vector<std::string> getSharedGenesInCluster() const { return genesInClusterWithBothMotifs; };
-    long getNumSharedGenesInUniverse() const { return genesInUniverseWithBothMotifs.size(); };
+  double getpValue() const { return pval; };
+  long getClusterSize() const { return clusterSize; };
+  std::vector<std::string> getSharedGenesInCluster() const { return genesInClusterWithBothMotifs; };
+  long getNumSharedGenesInUniverse() const { return genesInUniverseWithBothMotifs.size(); };
 
-    void findIntersectingGenes(motif &m1, motif &m2, double ICthreshold, std::unordered_map<std::string, int> &promSizes);
-    void colocTest(long universeSize, double ICthreshold, const std::string &clusterName, std::vector<std::string> &genesInCluster);
+  void findIntersectingGenes(motif &m1, motif &m2, double ICthreshold, std::unordered_map<std::string, int> &promSizes);
+  void colocTest(long universeSize, double ICthreshold, const std::string &clusterName,
+                 std::vector<std::string> &genesInCluster);
 
 private:
-    void reset();
-    bool motifInstancesOverlap(motif &m1, motif &m2, motifInstance &m1Instance, motifInstance &m2Instance, double IDthreshold);
-    bool geometricBinomialTest(const std::vector<bool> &motifLocationsToKeep, const std::string &gene, int promoterLength, motif &mt);
-    double geometricMean(std::vector<double>::iterator first, std::vector<double>::iterator last);
-    double binomialCDF(long numPVals, long numLocations, double gm);
+  void reset();
+  bool motifInstancesOverlap(motif &m1, motif &m2, motifInstance &m1Instance, motifInstance &m2Instance,
+                             double IDthreshold);
+  bool geometricBinomialTest(const std::vector<bool> &motifLocationsToKeep, const std::string &gene, int promoterLength,
+                             motif &mt);
+  double geometricMean(std::vector<double>::iterator first, std::vector<double>::iterator last);
+  double binomialCDF(long numPVals, long numLocations, double gm);
 
-    double pval;
-    long clusterSize;
+  double pval;
+  long clusterSize;
 
-    std::vector<std::string> genesInClusterWithBothMotifs;
-    std::vector<std::string> genesInUniverseWithBothMotifs;
+  std::vector<std::string> genesInClusterWithBothMotifs;
+  std::vector<std::string> genesInUniverseWithBothMotifs;
 };
 
 #endif /* motifComparison_hpp */
