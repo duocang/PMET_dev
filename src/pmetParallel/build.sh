463d86b4c95bc8cbfe586fde39820e16fece7067
Bai juyi <wangxuesong29@gmail.com>
2023-06-12 00:24:54 +0200

    add test data

--- src/pmetParallel/build.sh
+++ src/pmetParallel/build.sh
@@ -7,10 +7,31 @@ cd bin
 # 执行CMake命令生成Makefile
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 
-# 执行Make命令进行编译
 make
 
 sleep 1
 rm Makefile
 rm -rf CMake*
 rm -rf cmake_install.cmake
+
+cd ..
+
+# remove genes not present in pre-computed pmet index (universe.txt)
+grep -Ff test_data/universe.txt test_data/gene.txt > test_data/gene.txttemp
+
+# Run PMET
+bin/pmetParallel \
+    -d . \
+    -g test_data/gene.txttemp \
+    -i 4 \
+    -p test_data/promoter_lengths.txt \
+    -b test_data/binomial_thresholds.txt \
+    -c test_data/IC.txt \
+    -f test_data/fimohits \
+    -o test_result \
+    -t 2
+
+rm test_data/gene.txttemp
+
+cat test_result/*.txt > test_result/motif_output.txt
+rm test_result/temp*.txt
