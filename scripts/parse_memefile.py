12f460fcc50b9cb0fecdff2892755cdafe8cfac0
Bai juyi <wangxuesong29@gmail.com>
2024-03-25 19:11:33 +0100

    python: close file after writing

--- scripts/parse_memefile.py
+++ scripts/parse_memefile.py
@@ -7,23 +7,14 @@ parser.add_argument('file', type=argparse.FileType('r'))
 parser.add_argument('outdir', type=str)
 args = parser.parse_args()
 
-#keeping this here in case the docker turns out to work different
-#bigfile = np.asarray(args.file.read().splitlines())
+# keeping this here in case the docker turns out to work different
 bigfile = np.asarray(args.file.readlines())
-
-
-#motcount = 0
-#for i in range(len(bigfile)):
-#    if bigfile[i].find('MOTIF')>-1:
-#        motcount = motcount+1
-##now let's transform it into the actual number of zeros
-#motcount = int(np.floor(np.log10(motcount))+1)
 
 #time to find the header
 headind = 0
 while bigfile[headind].find('MOTIF')==-1:
     headind += 1
-#so ind is where the header ends and the MOTIFs start
+# so ind is where the header ends and the MOTIFs start
 ind1 = headind
 counter = 1
 for i in range((ind1+1),len(bigfile)):
@@ -33,16 +24,20 @@ for i in range((ind1+1),len(bigfile)):
         bigfile[ind1] = bigfile[ind1].upper()
         lines_to_write = bigfile[inds_to_write]
         motname = bigfile[ind1].split()[1]
-        writer = open(os.path.normcase(args.outdir+motname+'.txt'),'w')
-        writer.writelines(lines_to_write)
-        #the start of the new motif is here
+        # Use with statement to ensure the file is properly closed
+        with open(os.path.normcase(args.outdir + motname + '.txt'), 'w') as writer:
+            writer.writelines(lines_to_write)
+        # the start of the new motif is here
         ind1 = ind2
         counter = counter+1
-#we have a motif at the end
+
+# Handling the last motif
 ind2 = i+1
 inds_to_write = np.append(np.arange(headind),np.arange(ind1,ind2))
 bigfile[ind1] = bigfile[ind1].upper()
 lines_to_write = bigfile[inds_to_write]
 motname = bigfile[ind1].split()[1]
-writer = open(os.path.normcase(args.outdir+motname+'.txt'),'w')
-writer.writelines(lines_to_write)
+
+# Use with statement for the last motif as well
+with open(os.path.normcase(args.outdir + motname + '.txt'), 'w') as writer:
+    writer.writelines(lines_to_write)
